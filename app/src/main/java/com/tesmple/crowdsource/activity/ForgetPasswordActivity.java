568c553dbff54a8787459e77525eaca220d0de43
Yasic <2207793765@qq.com>
2015-10-10 09:18:56 +0800

    部分未更新

---
+++ app/src/main/java/com/tesmple/crowdsource/activity/ForgetPasswordActivity.java
@@ -0,0 +1,176 @@
+package com.tesmple.crowdsource.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.RequestMobileCodeCallback;
+import com.gc.materialdesign.views.ButtonRectangle;
+import com.tesmple.crowdsource.R;
+import com.tesmple.crowdsource.utils.EditTextUtils;
+
+/**
+ * Created by ESIR on 2015/10/9.
+ */
+public class ForgetPasswordActivity extends Activity{
+    /**
+     * 忘记密码界面的账号EditText
+     */
+    private AutoCompleteTextView forgetpasswordEtphone;
+
+    /**
+     * 忘记密码界面的新密码EditText
+     */
+    private AutoCompleteTextView forgetpasswordEtNewpassword;
+
+    /**
+     * 忘记密码界面的确认新密码EditText
+     */
+    private AutoCompleteTextView forgetpasswordEtConfirmpassword;
+
+    /**
+     * 忘记密码界面的验证码输入EditText
+     */
+    private AutoCompleteTextView forgetpasswordEtProvecode;
+
+    /**
+     * 忘记密码界面的获取验证码按钮
+     */
+    private ButtonRectangle forgetpasswordBtnGetProveCode;
+
+    /**
+     * 忘记密码界面的保存按钮
+     */
+    private ButtonRectangle forgetpasswordBtnSavePassword;
+
+    /**
+     * 忘记密码界面的主界面外部的ScrollView
+     */
+    private ScrollView forgetpasswordSvScrollform;
+
+    /**
+     * 忘记密码界面的Progressbar外部布局
+     */
+    private LinearLayout forgetpasswordLlProgressbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword);
+        initViewBind();
+        initToolbar();
+        setAutoCompleteTextViews();
+        setButton();
+    }
+
+    /**
+     * 初始化toolbar
+     */
+    private void initToolbar(){
+        Toolbar forgetPasswordToolbar = (Toolbar)findViewById(R.id.toolbar);
+        forgetPasswordToolbar.setTitle("修改密码");
+        forgetPasswordToolbar.setLogo(R.drawable.ic_back);
+    }
+
+    /**
+     * 控件绑定
+     */
+    private void initViewBind(){
+        forgetpasswordEtphone = (AutoCompleteTextView)findViewById(R.id.forgetpassword_et_phone);
+        forgetpasswordEtNewpassword = (AutoCompleteTextView)findViewById(R.id.forgetpassword_et_newpassword);
+        forgetpasswordEtConfirmpassword = (AutoCompleteTextView)findViewById(R.id.forgetpassword_et_confirmpassword);
+        forgetpasswordBtnGetProveCode = (ButtonRectangle)findViewById(R.id.forgetpassword_btn_getprovecode);
+        forgetpasswordBtnSavePassword = (ButtonRectangle)findViewById(R.id.forgetpassword_btn_savepassword);
+        forgetpasswordSvScrollform = (ScrollView)findViewById(R.id.forgetpassword_sv_scrollform);
+        forgetpasswordLlProgressbar = (LinearLayout)findViewById(R.id.forgetpassword_ll_progressbar);
+    }
+
+    /**
+     * 设置忘记密码界面的AutoCompleteTextView报错机制
+     */
+    private void setAutoCompleteTextViews(){
+        forgetpasswordEtphone.setError(null);
+        forgetpasswordEtNewpassword.setError(null);
+        forgetpasswordEtConfirmpassword.setError(null);
+    }
+
+    /**
+     * 设置button相关监听
+     */
+    private void setButton(){
+        forgetpasswordBtnGetProveCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attempGetProveCode();
+            }
+        });
+
+        forgetpasswordBtnSavePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    /**
+     * 尝试获取验证码
+     */
+    private void attempGetProveCode(){
+        String userPhone = forgetpasswordEtphone.getText().toString();
+        String userNewPassword = forgetpasswordEtNewpassword.getText().toString();
+        String userConfirmPassword = forgetpasswordEtConfirmpassword.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if( !EditTextUtils.isPhoneNumber(userPhone) ){
+            forgetpasswordEtphone.setError(getString(R.string.error_invalid_phone));
+            cancel = true;
+            focusView = forgetpasswordEtphone;
+        }else if( !EditTextUtils.isPassword(userNewPassword)){
+            forgetpasswordEtNewpassword.setError(getString(R.string.error_invalid_password));
+            cancel = true;
+            focusView = forgetpasswordEtNewpassword;
+        }else if( !userNewPassword.equals(userConfirmPassword) ){
+            forgetpasswordEtNewpassword.setError(getString(R.string.error_newpassword_notsame));
+            cancel = true;
+            focusView = forgetpasswordEtNewpassword;
+        }
+
+        if(cancel){
+            focusView.requestFocus();
+        }else {
+            forgetpasswordSvScrollform.setAlpha(0.5f);
+            forgetpasswordLlProgressbar.setVisibility(View.VISIBLE);
+            AVUser.requestPasswordResetBySmsCodeInBackground(userPhone, new RequestMobileCodeCallback() {
+                @Override
+                public void done(AVException e) {
+                    if (e == null) {
+                        //发送成功了
+                    } else if(e.getCode() == 211){
+                        Snackbar.make(forgetpasswordBtnSavePassword, R.string.error_phone_not_register, Snackbar.LENGTH_LONG).show();
+                    } else if(e.getCode() == AVException.USER_WITH_MOBILEPHONE_NOT_FOUND){
+                        Snackbar.make(forgetpasswordBtnSavePassword, R.string.error_phone_not_register,Snackbar.LENGTH_LONG).show();
+                    } else  {
+                        Snackbar.make(forgetpasswordBtnSavePassword,R.string.please_check_your_network,Snackbar.LENGTH_LONG).show();
+                    }
+
+                    Log.i("error",e.toString());
+                    forgetpasswordSvScrollform.setAlpha(1.0f);
+                    forgetpasswordLlProgressbar.setVisibility(View.GONE);
+                }
+            });
+        }
+    }
+}
